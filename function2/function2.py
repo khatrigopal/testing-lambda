e14d5129991c2464ca96a2b87f4be3c3438b3698
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-20 00:43:45 +0530

    Update function2.py

--- function2/function2.py
+++ function2/function2.py
@@ -1,3 +1,8 @@
 def lambda_handler (event, context):
    print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welcome to Second Lambda Fumction Pipeline")
   
