ae1b052a6bf101fb867ffba0b86df5d04fa41b26
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-20 00:45:13 +0530

    Update function1.py

--- function1/function1.py
+++ function1/function1.py
@@ -1,3 +1,13 @@
 def lambda_handler (event, context):
    print ("Welcome to Second Lambda Fumction Pipeline")
    print ("Welome to Persistent"}
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welome to Persistent"}
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welome to Persistent"}
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welome to Persistent"}
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welome to Persistent"}
+   print ("Welcome to Second Lambda Fumction Pipeline")
+   print ("Welome to Persistent"}
