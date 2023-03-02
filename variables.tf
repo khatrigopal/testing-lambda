51d6ca6b14dc4822d4be8a8a0ef6412e8172b060
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-02 19:35:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -61,3 +61,24 @@ variable "test_name" {
   type        = string
   default     = "khatrig"
 }
+
+################
+
+
+variable "layer_name" {
+  type    = string
+  default = "my-layer"
+}
+
+variable "layer_description" {
+  type    = string
+  default = "My Lambda layer"
+}
+
+variable "layer_bucket" {
+  type    = string
+}
+
+variable "layer_key" {
+  type    = string
+}
