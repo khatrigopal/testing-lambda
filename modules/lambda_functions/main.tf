2b01c78dc47fb3ed3c22542debf2e80f81269977
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-09 15:03:16 +0000

    Update main.tf

--- modules/lambda_functions/main.tf
+++ modules/lambda_functions/main.tf
@@ -6,11 +6,12 @@ resource "aws_lambda_function" "lambda_functions" {
   for_each        = { for c in var.function_configurations : c.function_name => c }
   function_name   = each.value.function_name
   #role            = "arn:aws:iam::558940753150:role/lambda-full-acces"
-  role            = "arn:aws:iam::558940753150:role/lambda_custom_role"
+  #role            = "arn:aws:iam::558940753150:role/lambda_custom_role"
+  role            = each.value.role
   handler         = each.value.handler
   #handler         = each.value.lamdba_handler  
   runtime         = each.value.runtime
-  layers =          [each.value.layers]
+  #layers =          [each.value.layers]
   memory_size        = each.value.memory_size
   source_code_hash = filebase64sha256(data.archive_file.lambda_functions[each.key].output_path)
   filename        = data.archive_file.lambda_functions[each.key].output_path
