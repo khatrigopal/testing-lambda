e9a3ff1ed393a05616eb43f0ba57607a591f1ce2
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-09 15:04:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,8 @@ module "lambda_functions" {
       runtime         = "python3.8"
       code_directory  = "function1"
       #layer_arn = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:3"
-      layers = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:3"
+      #layers = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:3"
+      role = "arn:aws:iam::558940753150:role/lambda_custom_role"
       memory_size        = 128
     },
     {
@@ -16,8 +17,9 @@ module "lambda_functions" {
       handler         = "function2.lambda_handler"
       runtime         = "python3.8"
       code_directory  = "function2"
+      role = "arn:aws:iam::558940753150:role/lambda_custom_role"
       #layer_arn = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:3"
-      layers = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:3"
+      #layers = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:3"
       memory_size        = 128
     }
   ]
