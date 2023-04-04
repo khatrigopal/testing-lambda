ee3bd15855e87900fccbb64816a014521b267423
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-04 11:45:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+#module "lambda_functions" {
+ # source = "./modules/lambda_functions"
+
+  #function_configurations = [
+   # {
+    #  function_name   = "function1"
+     # handler         = "function1.lambda_handler"
+     # runtime         = "python3.8"
+      #code_directory  = "function1"
+      # layers = data.terraform_remote_state.networking.outputs.layer_1_arn
+      #role = "arn:aws:iam::558940753150:role/lambda_custom_role"
+      #memory_size        = 128
+    #}
+    
+  #]
+#}
+
+
+#output "function1_arn" {
+ # value = module.lambda_functions.lambda_functions["function1"].arn
+#}
+  
+ 
 module "lambda_functions" {
   source = "./modules/lambda_functions"
 
@@ -8,17 +31,29 @@ module "lambda_functions" {
       runtime         = "python3.8"
       code_directory  = "function1"
       #layer_arn = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:3"
-      #layers = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_1:5"
-       layers = data.terraform_remote_state.networking.outputs.layer_1_arn
+      layers = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_1:5"
       role = "arn:aws:iam::558940753150:role/lambda_custom_role"
       memory_size        = 128
+    },
+    {
+      function_name   = "function2"
+      handler         = "function2.lambda_handler"
+      runtime         = "python3.8"
+      code_directory  = "function2"
+      role = "arn:aws:iam::558940753150:role/lambda_custom_role"
+      #layer_arn = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:3"
+      layers = "arn:aws:lambda:us-east-1:558940753150:layer:my_layer_2:5"
+      memory_size        = 128
     }
-    
   ]
 }
 
 
 output "function1_arn" {
   value = module.lambda_functions.lambda_functions["function1"].arn
+}
+
+output "function2_arn" {
+  value = module.lambda_functions.lambda_functions["function2"].arn
 }
 
